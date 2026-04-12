ff3846fee18c5895838e9c0c4ecf6e85a800397c
Aleksander Forsberg <forsberg518@gmail.com>
2026-04-13 00:20:19 +0200

    fix: use www.planra.no consistently - planra.no redirects to www

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 module.exports = function handler(req, res) {
   const params = new URLSearchParams({
     client_id: process.env.OAUTH_CLIENT_ID,
-    redirect_uri: 'https://planra.no/api/callback',
+    redirect_uri: 'https://www.planra.no/api/callback',
     scope: 'repo',
   });
   const githubUrl = `https://github.com/login/oauth/authorize?${params}`;
